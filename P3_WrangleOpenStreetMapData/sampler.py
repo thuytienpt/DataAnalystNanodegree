2ce068a9e8ca04d8e8330ddd9edc077c84c7f8cc
Tien Pham <thuytjen.pt@gmail.com>
2017-07-30 22:20:43 +0800

    Initial project 3

---
+++ P3_WrangleOpenStreetMapData/sampler.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+
+OSM_FILE = 'singapore.osm'
+SAMPLE_FILE = 'sample.osm'
+k = 50  # Parameter: take every k-th top level element
+
+
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag
+    Reference:
+    http://stackoverflow.com/questions/3095434/inserting-newlines-in-xml-file-generated-via-xml-etree-elementtree-in-python
+    """
+    context = iter(ET.iterparse(osm_file, events=('start', 'end')))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
+
+
+with open(SAMPLE_FILE, 'wb') as output:
+    output.write(bytes('<?xml version="1.0" encoding="UTF-8"?>\n', 'UTF-8'))
+    output.write(bytes('<osm>\n  ', 'UTF-8'))
+
+    for i, element in enumerate(get_element(OSM_FILE)):
+        if i % k == 0:
+            output.write(ET.tostring(element, encoding='utf-8'))
+
+    output.write(bytes('</osm>', 'UTF-8'))
